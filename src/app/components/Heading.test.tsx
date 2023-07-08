d699581ce9f08801936973752175a9b448f5926e
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-07-07 20:22:48 -0400

    Add first name input

--- src/app/components/Heading.test.tsx
+++ src/app/components/Heading.test.tsx
@@ -8,3 +8,11 @@ it("renders heading", () => {
   // Then
   expect(container.querySelector("h1")?.textContent).toEqual("Heading");
 });
+
+it("renders first name", () => {
+  // Given
+  const { container } = render(<Heading />);
+
+  // Then
+  expect(container.querySelector("#firstName")).toBeTruthy();
+});
--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -1,7 +1,15 @@
 export default function Heading() {
   return (
-    <>
+    <div className="container">
       <h1>Heading</h1>
-    </>
+      <form className="row g-3">
+        <div className="col-md-3">
+          <label htmlFor="firstName" className="form-label">
+            First Name
+          </label>
+          <input type="text" className="form-control" id="firstName" />
+        </div>
+      </form>
+    </div>
   );
 }
