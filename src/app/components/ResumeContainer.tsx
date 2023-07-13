971f1f0abd435ca6a7d66d43cc1bf81c3568d56c
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-07-13 19:51:23 -0400

    Rename state to heading form

--- src/app/components/ResumeContainer.tsx
+++ src/app/components/ResumeContainer.tsx
@@ -3,11 +3,11 @@ import Resume from "../types/Resume";
 import HeadingForm from "./HeadingForm";
 
 export default function ResumeContainer() {
-  const [showHeading, setShowHeading] = useState(false);
+  const [showHeadingForm, setShowHeadingForm] = useState(false);
   useEffect(() => {
     localStorage.getItem("resume")
       ? ""
       : localStorage.setItem("resume", JSON.stringify(new Resume()));
   }, []);
-  return <>{showHeading && <HeadingForm />}</>;
+  return <>{showHeadingForm && <HeadingForm />}</>;
 }
