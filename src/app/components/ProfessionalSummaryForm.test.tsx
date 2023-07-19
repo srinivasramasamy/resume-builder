4fd1c7f9163039f0ef834b9ae9c53721078a82ce
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-07-19 14:16:16 -0400

    Add professional summary form

---
+++ src/app/components/ProfessionalSummaryForm.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import ProfessionalSummaryForm from "./ProfessionalSummaryForm";
+
+describe("Professional Summary Form", () => {
+  it("should render professional summary form", () => {
+    // Given
+    const { queryByText } = render(<ProfessionalSummaryForm />);
+
+    // Then
+    expect(queryByText("Professional Summary")).toBeTruthy();
+  });
+});
---
+++ src/app/components/ProfessionalSummaryForm.tsx
@@ -0,0 +1,7 @@
+export default function ProfessionalSummaryForm() {
+  return (
+    <div className="container">
+      <h1>Professional Summary</h1>
+    </div>
+  );
+}
