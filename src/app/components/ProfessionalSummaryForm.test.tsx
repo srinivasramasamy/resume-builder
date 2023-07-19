3a2d884f3163ad39f510febf1c9ccd4d350abd46
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-07-19 14:39:01 -0400

    Update test name

--- src/app/components/ProfessionalSummaryForm.test.tsx
+++ src/app/components/ProfessionalSummaryForm.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import ProfessionalSummaryForm from "./ProfessionalSummaryForm";
 
 describe("Professional Summary Form", () => {
-  it("should render professional summary form", () => {
+  it("should render professional summary heading", () => {
     // Given
     const { queryByText } = render(<ProfessionalSummaryForm />);
 
