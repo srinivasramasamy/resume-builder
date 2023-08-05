76a8c724bd87ca2d6f81656c06b684d8efe891e2
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-08-05 15:32:51 -0400

    Update list component

--- src/app/components/List.test.tsx
+++ src/app/components/List.test.tsx
@@ -4,9 +4,28 @@ import List from "./List";
 describe("List", () => {
   it("should render list", () => {
     // Given
-    const { container } = render(<List />);
+    const { container } = render(<List items={[]} />);
 
     // Then
     expect(container).toBeTruthy();
+  });
+
+  it("should render items", () => {
+    // Given
+    const item1 = "Item1";
+    const items: string[] = [item1];
+    const { queryByText } = render(<List items={items} />);
+
+    // Then
+    expect(queryByText(item1)).toBeTruthy();
+  });
+
+  it("should render same number of items", () => {
+    // Given
+    const items: string[] = ["item1", "item2", "item3"];
+    const { getAllByRole } = render(<List items={items} />);
+
+    // Then
+    expect(getAllByRole("listitem").length).toBe(items.length);
   });
 });
--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -1,3 +1,13 @@
-export default function List() {
-  return <></>;
+interface IProps {
+  items: string[];
+}
+
+export default function List({ items }: IProps) {
+  return (
+    <ul>
+      {items?.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
 }
