e5e066fea9c83477f41892ba84016374d22b9404
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-08-06 20:12:32 -0400

    Add list to skills component

--- src/app/components/Skills.test.tsx
+++ src/app/components/Skills.test.tsx
@@ -1,12 +1,39 @@
 import { render } from "@testing-library/react";
+import Resume from "../types/Resume";
 import Skills from "./Skills";
 
 describe("Skills", () => {
+  beforeEach(() => {
+    jest
+      .spyOn(Storage.prototype, "getItem")
+      .mockReturnValue(JSON.stringify(new Resume()));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("render skills heading", () => {
     // Given
     const { container } = render(<Skills />);
 
     // Then
     expect(container.querySelector("h1")?.textContent).toEqual("Skills");
+  });
+
+  it("renders skills form local storage", () => {
+    // Given
+    const skills: string[] = ["Javascript", "React"];
+    jest
+      .spyOn(Storage.prototype, "getItem")
+      .mockReturnValue(JSON.stringify(new Resume({ skills: skills })));
+
+    // When
+    const { getAllByRole } = render(<Skills />);
+
+    // Then
+    expect(getAllByRole("listitem").map((li) => li.textContent)).toEqual(
+      skills
+    );
   });
 });
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,7 +1,13 @@
+import { useResume } from "../hooks/UseResume";
+import List from "./List";
+
 export default function Skills() {
+  const [resume] = useResume();
+
   return (
     <div className="container">
       <h1>Skills</h1>
+      <List items={resume.skills} />
     </div>
   );
 }
