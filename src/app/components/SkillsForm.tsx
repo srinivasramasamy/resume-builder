d84a1f6fa4ddbe4c10eeb760c1645dcfa0394ef1
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-08-08 20:28:07 -0400

    Add remove item method

--- src/app/components/SkillsForm.tsx
+++ src/app/components/SkillsForm.tsx
@@ -4,10 +4,12 @@ import List from "./List";
 export default function SkillsForm() {
   const [resume] = useResume();
 
+  const removeItem = (item: number) => {};
+
   return (
     <div className="container">
       <h1>Skills</h1>
-      <List items={resume.skills} />
+      <List items={resume.skills} removeItem={removeItem} />
     </div>
   );
 }
