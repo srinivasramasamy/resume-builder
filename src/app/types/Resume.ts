fadd5724a74fc6dda3aed4d19b3c3c107b20e7e4
Srinivas Ramasamy <srinivasramasami@gmail.com>
2023-09-27 06:42:50 -0400

    Add education to resume type

--- src/app/types/Resume.ts
+++ src/app/types/Resume.ts
@@ -1,17 +1,21 @@
+import Education from "./Education";
 import { Heading } from "./Heading";
 
 export default class Resume {
   heading: Heading;
   professionalSummary: string;
   skills: string[];
+  educations: Education[];
 
   constructor({
     heading = new Heading(),
     professionalSummary = "",
     skills = new Array<string>(),
+    educations = new Array<Education>(),
   } = {}) {
     this.heading = heading;
     this.professionalSummary = professionalSummary;
     this.skills = skills;
+    this.educations = educations;
   }
 }
