01b98b0e1f63ed40d82494ef521f0a2f245a6bbc
Srinivas Ramasamy <srinivasramasami@gmail.com>
2024-03-28 05:55:34 -0400

    Add jobs to resume class

--- src/app/types/Resume.ts
+++ src/app/types/Resume.ts
@@ -1,21 +1,25 @@
 import Education from "./Education";
 import { Heading } from "./Heading";
+import Job from "./Job";
 
 export default class Resume {
   heading: Heading;
   professionalSummary: string;
   skills: string[];
   educations: Education[];
+  jobs: Job[];
 
   constructor({
     heading = new Heading(),
     professionalSummary = "",
     skills = new Array<string>(),
     educations = new Array<Education>(),
+    jobs = new Array<Job>(),
   } = {}) {
     this.heading = heading;
     this.professionalSummary = professionalSummary;
     this.skills = skills;
     this.educations = educations;
+    this.jobs = jobs;
   }
 }
